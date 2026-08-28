18f1bab47416bb7b711445c4c144353548473ed3
HiblAntonio <74968697+HiblAntonio@users.noreply.github.com>
2026-08-28 22:22:03 +0200

    Delete src directory

--- src/components/common/SubmitButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SubmitButton({ children }) {
-  return (
-    <button type="submit" className="submit-policy-btn">
-      {children}
-    </button>
-  );
-}
